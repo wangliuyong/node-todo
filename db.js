a8d0ffdd9b24bd59d1e07b689193f9f89a9d823e
wly <1355498705@qq.com>
2021-09-04 23:49:03 +0800

    封装提取方法到db

---
+++ db.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const path = require("path");
+// home路径，兼容macOS  windows
+const homePath = process.env.HOME || require("os").homedir();
+// 构造存储数据的文件
+const dbPath = path.join(homePath, ".todo");
+
+/**
+ * @description  读取文件todo，没有就会创建todo（{flag:'a+'}）
+ * @param {homePath} path home的路径
+ * @returns Promise
+ */
+module.exports.read = (path = homePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(dbPath, { flag: "a+" }, async (error, data) => {
+      if (error) {
+        reject(error);
+      } else {
+        let list;
+        try {
+          list = JSON.parse(data.toString());
+          console.log(1,list)
+        } catch (error) {
+          list = [];
+        }
+        // console.log(2,list)
+        resolve(list);
+      }
+    });
+  });
+};
+
+/**
+ * @description 将内容写入文件
+ * @param {Array} list 
+ * @param {homePath} path 
+ * @returns Promise
+ */
+module.exports.write = (list, path = homePath) => {
+  return new Promise((resolve, reject) => {
+    const strings = JSON.stringify(list);
+    fs.writeFile(dbPath, strings, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
--- index.js
+++ index.js
@@ -1,43 +1,10 @@
-const fs = require('fs')
-const path = require('path')
+const {read, write} = require('./db.js')
 
-// home路径，兼容macOS  windows
-const homePath = process.env.HOME || require('os').homedir()  
-// 构造存储数据的文件
-const dbPath = path.join(homePath,'.todo')
-
-module.exports.add = (title) => {
-  // 读取文件todo，没有就会创建todo（{flag:'a+'}）
-  fs.readFile(dbPath,{flag:'a+'}, (err1, data) => {
-    if (err1) {
-      console.log(err1)
-    } else {
-      let list
-      try {
-        list = JSON.parse(data.toString())
-      } catch (error) {
-        list = []
-      }
-
-      const task = {
-        title,
-        done: false
-      }
-
-      list.push(task)
-      
-      const strings = JSON.stringify(list) 
-
-      fs.writeFile(dbPath,strings,(err2) => {
-        console.log(err2)
-      })
-
-    }
-    
-
-
-
-
-    console.log(data.toString());
-  });
+module.exports.add =  async(title) => {
+  const list = await read()
+  list.push({
+    title,
+    done: false
+  })
+  write(list)
 }
