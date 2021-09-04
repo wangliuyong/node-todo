adbba27f294f9a8712063757b70bc476b69d4c01
wly <1355498705@qq.com>
2021-09-04 21:56:23 +0800

    初始化项目引入commander.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const { program } = require('commander');
+program.version('0.0.1');
+
+
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('-s, --small', 'small pizza size')
+  .option('-p, --pizza-type <type>', 'flavour of pizza');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
