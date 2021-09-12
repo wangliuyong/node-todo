92473203457025bef7784e396e836a0a1634fe36
wly <1355498705@qq.com>
2021-09-12 18:56:26 +0800

    新建测试文件夹，增加db的读写测试用例

---
+++ _test_/db.spec.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+const db = require('../db')
+
+describe('db',() => {
+  it('can read',() => {
+    expect(db.read instanceof Function).toBe(true);
+  })
+
+  it('can write',() => {
+    expect(db.write instanceof Function).toBe(true);
+  })
+})
